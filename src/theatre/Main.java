c02526782c1fe3c726b6c7caf32c734fb612a5ed
bwielk <bwielk@gmail.com>
2018-03-03 00:01:48 +0100

    Introducing some Collections methods - reverse() and shuffle()

--- src/theatre/Main.java
+++ src/theatre/Main.java
@@ -3,14 +3,23 @@ package theatre;
 import java.awt.List;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
 	
 	public static void main(String[] args){
 		Theatre theatre = new Theatre("Cameo", 2, 10);
-		Collection<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+		//Array with exactly the same data like the one in the instance of Theatre.theatre
+		ArrayList<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
 		printList(seatCopy);
 		
+		System.out.println("/////////////////////REVERSED////////////////////////");
+		Collections.reverse(seatCopy);
+		printList(seatCopy);
+		
+		System.out.println("/////////////////////SHUFFLED////////////////////////");
+		Collections.shuffle(seatCopy);
+		printList(seatCopy);
 		/*
 		theatre.getSeats();
 		if(theatre.reserveSeat("B05")){
@@ -26,7 +35,6 @@ public class Main {
 		}
 		*/
 	}
-	
 	public static void printList(Collection<Theatre.Seat> list){
 		for(Theatre.Seat seat : list){
 			System.out.println(" " + seat.getSeatNumber());

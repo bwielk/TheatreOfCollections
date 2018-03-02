ef159fe4b14b31a7a152e435a511bb6196739eda
bwielk <bwielk@gmail.com>
2018-03-03 00:15:33 +0100

    Introducting Collections methods - sort() method

--- src/theatre/Main.java
+++ src/theatre/Main.java
@@ -20,6 +20,19 @@ public class Main {
 		System.out.println("/////////////////////SHUFFLED////////////////////////");
 		Collections.shuffle(seatCopy);
 		printList(seatCopy);
+		
+		System.out.println("/////////////////////MIN AND MAX VALUE////////////////////////");
+		Theatre.Seat minSeat = Collections.min(seatCopy);
+		Theatre.Seat maxSeat = Collections.max(seatCopy);
+		System.out.println("MIN SEAT : " + minSeat.getSeatNumber());
+		System.out.println("MAX SEAT : " + maxSeat.getSeatNumber());
+		
+		System.out.println("/////////////////////SORTING////////////////////////");
+		Collections.shuffle(seatCopy);
+		printList(seatCopy);
+		sortList(seatCopy);
+		System.out.println("After SORTING");
+		printList(seatCopy);
 		/*
 		theatre.getSeats();
 		if(theatre.reserveSeat("B05")){
@@ -38,6 +51,19 @@ public class Main {
 	public static void printList(Collection<Theatre.Seat> list){
 		for(Theatre.Seat seat : list){
 			System.out.println(" " + seat.getSeatNumber());
+		}
+	}
+	
+	public static void sortList(ArrayList<Theatre.Seat> list){
+		for(int i=0; i<list.size() -1; i++){
+			for(int j=0; j<list.size(); j++){
+				//ascending sorting
+				if(list.get(i).compareTo(list.get(j)) <0){
+				//descending sorting
+			////if(list.get(i).compareTo(list.get(j)) >0){
+					Collections.swap(list, i, j);
+				}
+			}
 		}
 	}
 }

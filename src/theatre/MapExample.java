08a3bedd9e882cf568ec973cc1b72d38716b9a76
bwielk <bwielk@gmail.com>
2018-03-04 00:15:05 +0100

    More HashMap manipulating methods examples shown

--- src/theatre/MapExample.java
+++ src/theatre/MapExample.java
@@ -11,6 +11,13 @@ public class MapExample {
 		languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code");
 	
 		System.out.println(languages.get("Java"));
+		
+		System.out.println("////////////////////MANIPULATION////////////////////");
+		languages.remove("Java", "Complex and very good language");
+		languages.replace("Java", "My favourite language");
+		languages.replace("Java", "My least favourite language", "A compiled and hight level object oriented language");
+		for(String key : languages.keySet()){
+			System.out.println(key + " : " + languages.get(key));
+		}
 	}
-
 }
